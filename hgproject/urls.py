e9ec655a7e1572503ca96673884de97598b99621
hs-2091054-soldamlee <102890749+hs-2091054-soldamlee@users.noreply.github.com>
2022-11-18 09:14:41 +0900

    first page

--- hgproject/urls.py
+++ hgproject/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from firstpageapp import views
+from myapp import views
+#from myapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', name='firstpage', views.home), # 제일 먼저 보여지는 페이지에 대한 url
-    path(''),
+    path('', views.home, name='firstpage'), # 제일 먼저 보여지는 페이지에 대한 url
+    #path('secondpage/', name="secondpage",views.writingbutton), #인사하러가기 버튼을 눌렀을 때 2페이지로 이동
 ]
---
+++ myapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# 제일 먼저 보이는 첫페이지 보여주는 함수
+def home(request):
+    return render(request, "firstpage.html")
+
+# 첫페이지의 <인사하러 가기> 버튼을 눌렀을 때 실행되는 함수
+# 버튼 클릭 -> 2페이지로 가기
+def writingbutton(request):
+    return render()
