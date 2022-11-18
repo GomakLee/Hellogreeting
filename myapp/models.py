d5f86839780bdcd4a1b2b322da21eabbaf009971
hs-2091054-soldamlee <102890749+hs-2091054-soldamlee@users.noreply.github.com>
2022-11-18 18:28:11 +0900

    models.py littlebit

--- myapp/models.py
+++ myapp/models.py
@@ -1,12 +1,7 @@
 from django.db import models
 
 #추천 인삿말 데이터 리스트
-'''
-class MyModel(models.Model):
-    myList = models.TextField( null = True)
-    
-    import simplejson as json
 
-    MyModel = MyModel()
-    
-'''
+class MyModel(models.Model):
+    short = models.TextField(null = True, max_length=10) 
+    long = models.TextField(null = True, max_length=50)
