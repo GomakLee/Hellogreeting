8e8c65d840db94455fa3b40eb54e2613e20eae90
Baejb <bjb6478@naver.com>
2022-11-18 20:07:49 +0900

    카테고리 페이지 추가 , css 파일 ,이미지 추가

--- myapp/views.py
+++ myapp/views.py
@@ -6,9 +6,13 @@ def home(request):
 
 # 첫페이지의 <인사하러 가기> 버튼을 눌렀을 때 실행되는 함수
 # 버튼 클릭 -> 2페이지로 가기
-def gotosecondpage(request,post_id):
-    post_detail = get_object_or_404()
-    return render(request, "secondpage_base.html",{'post_detail':post_detail})
+# def gotosecondpage(request,post_id):
+#     post_detail = get_object_or_404()
+#     return render(request, "secondpage_base.html",{'post_detail':post_detail})
+
+def gotosecondpage(request):
+    return render(request, "secondpage_base.html")
+
 
 def gotothirdpage(request):
     return render(request, )
