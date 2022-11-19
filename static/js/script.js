64f885304b713445de1aca26525965f2970280a2
Baejb <bjb6478@naver.com>
2022-11-19 11:47:33 +0900

    버튼 이벤트 추가, 연말연시 페이지 추가

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,85 @@
 function newPage()  {
-    window.location.href = 'https://hianna.tistory.com'
+    window.location.href = 'http://127.0.0.1:5500/CategorySelect/Christmas.html'
 }
-// 클릭시 페이지 이동 링크는 추후 변경해야됨 .
+// 클릭시 페이지 이동
+let whoSend
+function bringClickValue(arg0){
+  var value = $(arg0).val();
+  whoSend = value;
+
+
+}
+
+function changeWho(){ // 누구에게 보낼지 변경
+  let whois = document.getElementById('who');
+  whois.innerHTML = whoSend;
+
+}
+
+
+
+// 버튼 클릭시 스타일 변경
+//첫 번째 버튼
+var category = document.getElementsByClassName("category");
+
+      function handleClick1(event) {
+        console.log(event.target);
+        // console.log(this);
+        // 콘솔창을 보면 둘다 동일한 값이 나온다
+
+        console.log(event.target.classList);
+
+        if (event.target.classList[1] === "clicked") {
+
+          event.target.classList.remove("clicked");
+        } else {
+          for (var i = 0; i < category.length; i++) {
+
+            category[i].classList.remove("clicked");
+          }
+
+          event.target.classList.add("clicked");
+        }
+      }
+
+      function init() {
+        for (var i = 0; i < category.length; i++) {
+            flag = 1 ;
+            category[i].addEventListener("click", handleClick1);
+        }
+      }
+
+      init();
+//두 번째 버튼
+var sendTo = document.getElementsByClassName("sendTo");
+
+
+      function handleClick2(event) {
+        changeWho();
+        console.log(event.target);
+        // console.log(this);
+        // 콘솔창을 보면 둘다 동일한 값이 나온다
+
+        console.log(event.target.classList);
+
+        if (event.target.classList[1] === "clicked1") {
+          event.target.classList.remove("clicked1");
+        } else {
+          for (var i = 0; i < sendTo.length; i++) {
+            sendTo[i].classList.remove("clicked1");
+          }
+
+          event.target.classList.add("clicked1");
+        }
+      }
+
+      function init1() {
+        for (var i = 0; i < sendTo.length; i++) {
+          sendTo[i].addEventListener("click", handleClick2);
+
+        }
+      }
+
+init1();
+
+
